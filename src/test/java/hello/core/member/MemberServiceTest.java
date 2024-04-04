1e8ccffaeef17bf60dfe31da3a1ddcf04ae49e8e
Hyelim Nam <nhlnhl@naver.com>
2024-04-04 20:48:06 +0900

    불필요한 public 제거

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -5,12 +5,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class MemberServiceTest {
+class MemberServiceTest {
 
     MemberService memberService;
 
     @BeforeEach
-    public void beforeEach() {
+    void beforeEach() {
         AppConfig appConfig = new AppConfig();
         memberService = appConfig.memberService();
     }
