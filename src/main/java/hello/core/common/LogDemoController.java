c39d7e9f1bfe7e5b49c01bcdeba2d0509c94d49b
Hyelim Nam <nhlnhl@naver.com>
2024-04-10 14:12:06 +0900

    proxyMode = ScopedProxyMode.TARGET_CLASS 추가

--- src/main/java/hello/core/common/LogDemoController.java
+++ src/main/java/hello/core/common/LogDemoController.java
@@ -2,7 +2,6 @@ package hello.core.common;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,13 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
